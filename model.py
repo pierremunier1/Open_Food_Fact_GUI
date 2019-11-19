aaaf9f97620840b815d7e568180221cb8e2103a8
pierro145 <pierremunier@hotmail.fr>
2019-11-19 18:07:19 +0100

    .

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ class Sqlconnection:
 
     def __init__(self):
         
-        
         self.variable_tablename = config.TABLE_PRODUCT
         self.db_infos_user = config.USER
         self.db_infos_pwd = config.PWD
@@ -33,13 +32,15 @@ class Sqlconnection:
 
             
         table = Table(self.variable_tablename, self.metadata,
-                Column('user_id', Integer),
-                Column('first_name', String(150)),
-                Column('last_name', String(150)),
-                Column('last_TEST', String(150)),    
+                Column('id', Integer),
+                Column('product_name', String(150)),
+                Column('ingredient', String(150)),
+                Column('nutriscore', String(1)),    
                 schema=self.db_infos_db)
     
         if table_exist == True:
             table.drop(self.engine)
+            print('Delete existing table..')
 
         table.create(self.engine)
+        print('Table succesfully created!')
